fc8552c63c6eadc412bb0026a1c5d4ecbee5109e
suvarna <saisuvarnay@gmail.com>
2024-06-06 15:57:24 +0530

    db env variables

--- backend/drf_IDES/settings.py
+++ backend/drf_IDES/settings.py
@@ -63,16 +63,30 @@ WSGI_APPLICATION = 'drf_IDES.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'IDES_ProductDetails',
+#         'HOST': '127.0.0.1',
+#         'PORT': '3306',
+#         'USER': 'suvarna',
+#         'PASSWORD': 'suvarna',
+#     }
+# }
+
+import os
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'IDES_ProductDetails',
-        'HOST': '127.0.0.1',
-        'PORT': '3306',
-        'USER': 'suvarna',
-        'PASSWORD': 'suvarna',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT', '3306'),
     }
 }
+
 
 
 
